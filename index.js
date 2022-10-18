171f0b8106e16af7d22f37502f4d6b91c37cd079
MaryyamSalah <maryyamslah20@gmail.com>
2022-10-18 15:57:04 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var a, b
+function add(a , b) {
+    return (a + b)
+  }
+  add()
+
+function subtract(a , b) {
+    return (a - b)
+  }
+  subtract()
+
+  function multiply(a , b) {
+    return (a * b)
+  }
+  multiply()
+
+  function divide (a , b) {
+    return (a / b)  
+  }
+  divide()
+
+  function increment (a,b){
+    return (a + 1)
+  }
+  increment()
+
+  function decrement (a,b){
+    return (a - 1)
+  }
+  decrement()
+
+  function makeInt(n){
+    return parseInt(n);
+  }
+  makeInt()
+
+  function makeInt(n){
+    return parseInt(n , 10);
+  }
+  makeInt()
+
+  function preserveDecimal(n){
+    return parseFloat(n)
+  }
+  preserveDecimal()
